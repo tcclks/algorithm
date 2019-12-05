e9aed3856054f0fadb5e05557eec2144102ad75d
tcclks <tcclkes@gmail.com>
2019-12-05 09:31:01 +0800

    removeDuplicates
    removeDuplicates

--- removeDuplicates.java
+++ removeDuplicates.java
@@ -1,6 +1,9 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         // 双指针法
+        if (nums.length == 0) {
+            return 0;
+        }
         int i = 0;
         for (int j = 1; j < nums.length; j++) {
             if (nums[j] != nums[i]) {
